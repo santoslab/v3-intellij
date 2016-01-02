06efadff5eb134e75954d4d8f89b67b5d43edd19
Robby <robby@k-state.edu>
2016-01-01 21:57:44 -0600

    Initial commit.

---
+++ src/org/sireum/intellij/SireumApplicationComponent.scala
@@ -0,0 +1,109 @@
+/*
+ Copyright (c) 2016, Robby, Kansas State University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.sireum.intellij
+
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.components._
+import java.io._
+import com.intellij.openapi.fileChooser._
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.Consumer
+import org.sireum.util.jvm.Exec
+
+object SireumApplicationComponent {
+  private var _sireumHome: Option[File] = None
+
+  final def sireumHome(project: Project): Option[File] = {
+    val pc = PropertiesComponent.getInstance
+    checkSireumDir(pc.getValue("org.sireum"))
+    if (_sireumHome.nonEmpty) return _sireumHome
+    val env = System.getenv("SIREUM_HOME")
+    if (checkSireumDir(env).isEmpty) {
+      var pathOpt: Option[String] = None
+      val desc = FileChooserDescriptorFactory.createSingleFolderDescriptor()
+      desc.setTitle("Select Sireum v3 directory")
+      FileChooser.chooseFile(
+        desc,
+        project, null, new Consumer[VirtualFile] {
+          override def consume(t: VirtualFile): Unit = {
+            pathOpt = Some(t.getCanonicalPath)
+          }
+        })
+      pathOpt.foreach(path =>
+        if (checkSireumDir(path).isEmpty)
+          Messages.showMessageDialog(project, "Sireum Not Found",
+            s"""$path does not seem to contain a working Sireum installation.
+               |Make sure to run Sireum at least once from the command-line.
+                       """.stripMargin, null)
+      )
+    }
+    _sireumHome
+  }
+
+  def runSireum(project: Project, input: Option[String], args: String*): Option[String] =
+    sireumHome(project) match {
+      case Some(d) => runSireum(d.getAbsolutePath, input, args)
+      case _ => None
+    }
+
+  private def runSireum(path: String, input: Option[String], args: Seq[String]): Option[String] = {
+    new Exec().run(0,
+      Seq(s"$path/platform/java/bin/java", "-jar",
+        s"$path/jvm/target/scala-2.11/sireum.jar") ++ args,
+      input, ("SIREUM_HOME", path)) match {
+      case Exec.StringResult(s, _) => Some(s)
+      case _ => None
+    }
+  }
+
+  private final def checkSireumDir(path: String): Option[File] = {
+    if (path == null) return None
+    runSireum(path, None, Seq()) match {
+      case Some(s) =>
+        if (s.lines.exists(
+          _.trim == "Sireum: A Software Analysis Platform (v3)")) {
+          val pc = PropertiesComponent.getInstance
+          pc.setValue("org.sireum", path)
+          _sireumHome = Some(new File(path))
+          _sireumHome
+        } else None
+      case _ => None
+    }
+  }
+}
+
+class SireumApplicationComponent extends ApplicationComponent {
+
+  override val getComponentName: String = "Sireum"
+
+  override def initComponent(): Unit = {
+  }
+
+  override def disposeComponent(): Unit = {
+  }
+}
---
+++ src/org/sireum/intellij/logika/action/LogikaCheckAction.scala
@@ -0,0 +1,102 @@
+/*
+ Copyright (c) 2016, Robby, Kansas State University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.sireum.intellij.logika.action
+
+import com.intellij.openapi.actionSystem._
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util._
+import org.sireum.intellij.SireumApplicationComponent
+
+object LogikaCheckAction {
+  val icon = IconLoader.getIcon("/logika.png")
+  val fileExts = Set(".sc", ".scala", ".logika", ".lgk")
+
+  def apply(): Unit = {
+    new LogikaCheckAction
+
+  }
+}
+
+import LogikaCheckAction._
+
+private class LogikaCheckAction
+  extends AnAction("Check Logika Proof",
+    "Check Logika Proof", LogikaCheckAction.icon) {
+
+  // init
+  {
+    val am = ActionManager.getInstance
+
+    val editorPopupGroup = am.getAction(IdeActions.GROUP_EDITOR_POPUP).
+      asInstanceOf[DefaultActionGroup]
+    editorPopupGroup.addAction(this, Constraints.LAST)
+
+    val runGroup = am.getAction(IdeActions.GROUP_RUN).
+      asInstanceOf[DefaultActionGroup]
+    runGroup.addSeparator()
+    runGroup.addAction(this, Constraints.LAST)
+  }
+
+  override def update(e: AnActionEvent): Unit = {
+    val project = e.getProject
+    e.getPresentation.setEnabled(project != null &&
+      fileExts.contains(getFileExt(project)))
+  }
+
+  def getFileExt(project: Project): String = {
+    val fem = FileEditorManager.getInstance(project)
+    fem.getSelectedFiles match {
+      case Array(f, _*) =>
+        val name = f.getName
+        val i = name.lastIndexOf('.')
+        if (i >= 0)
+          name.substring(name.lastIndexOf('.'))
+        else ""
+      case _ => ""
+    }
+  }
+
+  override def actionPerformed(e: AnActionEvent): Unit = {
+    e.getPresentation.setEnabled(false)
+    val input = Some(FileEditorManager.getInstance(e.getProject).
+      getSelectedTextEditor.getDocument.getText)
+    val isProgramming =
+      getFileExt(e.getProject) match {
+        case ".scala" | ".sc" => true
+        case _ => false
+      }
+    SireumApplicationComponent.runSireum(
+      e.getProject, input, "logika",
+      if (isProgramming) "--ideprog" else "--ide") match {
+      case Some(s) =>
+        println(s)
+      case _ =>
+    }
+
+    e.getPresentation.setEnabled(true)
+  }
+}

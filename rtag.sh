67a43682cd73298f563d98bd621395d84f70fe3d
Robby <robby@k-state.edu>
2020-06-13 14:05:16 -0500

    Added release tag script.

---
+++ rtag.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+VER=$(git log -n 1 --date=format:%Y%m%d.%H%M --pretty=format:3.%cd.%h)
+7z x sireum-v3-intellij.zip sireum-v3-intellij/lib/sireum-V3-intellij.jar
+7z x sireum-v3-intellij/lib/sireum-v3-intellij.jar META-INF/plugin.xml
+sed -i.bak "s/3.X.Y/${VER}/g" META-INF/plugin.xml
+7z a sireum-v3-intellij/lib/sireum-v3-intellij.jar META-INF/plugin.xml
+7z a sireum-v3-intellij.zip sireum-v3-intellij/lib/sireum-v3-intellij.jar
+rm -fR META-INF sireum-v3-intellij
+echo "Tagging ${VER}"
+git tag ${VER}
+git push origin ${VER}
